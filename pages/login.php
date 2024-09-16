863d4ed18d778a8105107740411a42e799440d89
Rolon con peluca <164172797+Spongiboby@users.noreply.github.com>
2024-09-16 14:41:38 -0300

    Nuevo login

--- pages/login.php
+++ pages/login.php
@@ -9,7 +9,30 @@
 </head>
 
 <body>
-
+    <div class="container">
+       <!-- Formulario de Inicio de Sesión -->
+       <div class="Login_Duenio_container">
+           <h2>Iniciar Sesión 🔒</h2>
+           <?php
+           // Verificar si hay un mensaje de registro exitoso
+           if (isset($_GET['registro']) && $_GET['registro'] === 'exitoso') {
+               echo "<script>alert('Registro exitoso');</script>";
+           }
+           ?>
+           <form action="backend_Login.php" method="POST">
+              <div class="input-group">
+                <label for="login_username">Usuario 🔓</label>
+                <input type="text" id="login_username" name="username" required>
+              </div>
+              <div class="input-group">
+                <label for="login_password">Contraseña 🔑</label>
+                <input type="password" id="login_password" name="password" required>
+              </div>
+              <button type="submit">Ingresar</button>
+           </form>
+           <p>¿No estás registrado? <a href="Register.html">Hazlo aquí</a></p>
+       </div>
+   </div>
 </body>
 
 </html>
